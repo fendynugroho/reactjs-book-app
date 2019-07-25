f183a1087f896024fa05c87ac2dc04d161944034
Fendy <fendynugroho@icloud.com>
2019-07-26 03:10:28 +0700

    finish login form, add inline error

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import LoginForm from '../forms/LoginForm';
 
-const LoginPage = () => <div>This is Login Page!</div>;
+const LoginPage = () => (
+	<div>
+		This is Login Page!
+		<LoginForm />
+	</div>
+);
 
 export default LoginPage;
