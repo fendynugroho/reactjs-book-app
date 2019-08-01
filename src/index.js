f566f7aff4fe9431e70b2971b0da23b488c1bcf4
Fendy <fendynugroho@icloud.com>
2019-08-02 01:21:24 +0700

    Login/Logout

--- src/index.js
+++ src/index.js
@@ -9,11 +9,17 @@ import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import rootReducer from './rootReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { userLoggedIn } from './actions/auth';
 
 const store = createStore(
 	rootReducer,
 	composeWithDevTools(applyMiddleware(thunk))
 );
+
+if (localStorage.bookJWT) {
+	const user = { token: localStorage.bookJWT };
+	store.dispatch(userLoggedIn(user));
+}
 
 ReactDOM.render(
 	<BrowserRouter>
